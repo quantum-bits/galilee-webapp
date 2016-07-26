8683136655ffa431699b575214e4264f4760a651
Ken Kiers <knkiers@taylor.edu>
2016-07-26 17:13:34 -0400

    add e2e test for adding text to existing advice for existing practice

--- e2e/admin-edit-reading-resources.e2e-spec.ts
+++ e2e/admin-edit-reading-resources.e2e-spec.ts
@@ -6,16 +6,23 @@ import { expectToMatch, getBody } from './utils';
 describe('admin edit reading resources page', function() {
   let page: AdminEditReadingResourcesPage;
   let numberPracticesOriginal: any;
+  let originalPracticeAdvice: any;
 
   beforeEach(() => {
     page = new AdminEditReadingResourcesPage();
-    //browser.driver.manage().window().setSize(1280, 1024);
     page.navigateTo();
     page.countPracticesInCard().then(
       // putting this here ensures that the promise gets resolved before continuing:
       // http://stackoverflow.com/questions/21685161/how-to-expect-dynamic-count-of-elements-in-e2e-tests-using-protractor
       number => numberPracticesOriginal = number
-    )
+    );
+    page.clickFirstCollapsiblePracticesDiv(); // open up the first collapsible div
+    page.fetchAdviceForFirstPractice().then(
+      advice => {
+        originalPracticeAdvice = advice;
+        page.clickFirstCollapsiblePracticesDiv(); // close the div
+      }
+    );
   });
 
   it('should show headers for First Reading, Practices and Resources', () => {
@@ -45,23 +52,11 @@ describe('admin edit reading resources page', function() {
     // NOTE: expect() forces the resolution of the promise....
     expect(page.countPracticesInCard()).toEqual(numberPracticesOriginal+1); // check that # of practices in card has increased by 1
     page.clickLastCollapsiblePracticesDiv(); // open up the collapsible div for the new practice
-    page.writeTextToPracticesTextArea('here is some test advice!'); // add some text
-
-    console.log('about to save');
-    page.saveText(); //save the new text
-    console.log('done saving; opening div');
+    page.writeTextToLastPracticesTextArea('here is some test advice!'); // add some text
+    page.saveTextLastPractice(); //save the new text
     page.clickLastCollapsiblePracticesDiv(); // open it up again
 
-    console.log('checking saved advice');
-    expect(page.checkSavedText()).toEqual('here is some test advice!'); // check that the saved text is the same as what was entered
-    console.log('deleting last practice');
-
-    //browser.pause();
-
-    //element(by.id('practicesCard')).all(by.css('.collapsible .collapsible-header')).last().element(by.tagName('button')).click();
-
-    //browser.pause();
-
+    expect(page.checkSavedTextLastPractice()).toEqual('here is some test advice!'); // check that the saved text is the same as what was entered
 
     page.attemptLastPracticeDelete();//click the delete button for the new practice; should launch a modal
 
@@ -69,51 +64,37 @@ describe('admin edit reading resources page', function() {
     // the active one will have text associated with it; in the following, we check...
     //  - that there is exactly one modal active
     //  - that the modal contains certain text
-    // ...and then we find the delete button on this modal and click it
+    // ...and then we find the delete button on this modal and click it;
+    // could probably put some of the following in the page object, but it's a bit tricky
+    // with the promises, etc.
     let modalArray = page.getModalsOnPage();
     var activeModalIndex: any;
     var numberModalsLaunched = 0;
     modalArray.getText().then(textArray => {
-      console.log('got here');
-      console.log(textArray);
-      console.log (textArray.length);
       for (var i=0; i< textArray.length; i++) {
-        if (textArray[i] !=='') {
-          console.log('we have a non-zero entry');
-          console.log(i);
+        if (textArray[i] !=='') { // non-empty entry...this is the active modal
           activeModalIndex = i;
           numberModalsLaunched++;
           expectToMatch(textArray[i], /Deleting this practice will also delete the associated advice, if any./);
         }
       }
-      console.log('number modals launched: ');
-      console.log(numberModalsLaunched);
       expect(numberModalsLaunched).toEqual(1);
-      console.log('about to delete');
 
-      // see: http://stackoverflow.com/questions/30862405/element-is-not-clickable-at-point-protractor
-      // apparently protractor finds the on-screen coordinates for a given button, but
-      // if there is an animation or a drop-down menu obscuring the button, the given
-      // coordinates might not be 'click-able'; the following makes sure that we
-      // wait until the button is clickable before attempting to click it
-      var EC = protractor.ExpectedConditions;
       var el = modalArray.get(activeModalIndex).element(by.linkText('DELETE'));
-      var isClickable = EC.elementToBeClickable(el);
+      page.clickWithWait(el);
 
-      console.log('waiting for delete button to be clickable');
-      browser.wait(isClickable, 5000);
-      console.log('done waiting');
-      el.click();
-      console.log('done deleting');
-      //browser.pause();
-      console.log('checking number practices again');
       expect(page.countPracticesInCard()).toEqual(numberPracticesOriginal); // number of practices should be back to the original value now
-
-
     });
 
+  });
 
-
+  it('should be able to add text to existing advice for existing practice and save it', () => {
+    page.clickFirstCollapsiblePracticesDiv(); // open up the first collapsible div
+    page.clickEditPracticeButton(); // click edit
+    page.writeTextToFirstPracticesTextArea('here is some additional advice!'); // add some text
+    page.saveTextFirstPractice(); //save the revised text
+    page.clickFirstCollapsiblePracticesDiv(); // open up the div again
+    expect(page.checkSavedTextFirstPractice()).toEqual(originalPracticeAdvice+'here is some additional advice!'); // check that the saved text is the original text plus the new text
   });
 
 
--- e2e/admin-edit-reading-resources.po.ts
+++ e2e/admin-edit-reading-resources.po.ts
@@ -20,32 +20,67 @@ export class AdminEditReadingResourcesPage {
     element.all(by.id('addPracticeDropdownLink')).first().click();
   }
 
+  clickFirstCollapsiblePracticesDiv() {
+    element(by.id('practicesCard')).all(by.css('.collapsible-header')).first().click();
+  }
+
   clickLastCollapsiblePracticesDiv() {
     element(by.id('practicesCard')).all(by.css('.collapsible-header')).last().click();
   }
 
-  writeTextToPracticesTextArea(text){
+  writeTextToLastPracticesTextArea(text){
     element(by.id('practicesCard')).all(by.tagName('textarea')).last().sendKeys(text)
   }
 
-  saveText(){
+  writeTextToFirstPracticesTextArea(text){
+    element(by.id('practicesCard')).all(by.tagName('textarea')).first().sendKeys(text)
+  }
+
+  /*
+  saveTextLastPractice(){
     // see: http://stackoverflow.com/questions/30862405/element-is-not-clickable-at-point-protractor
     var EC = protractor.ExpectedConditions;
     var el = element(by.id('practicesCard')).all(by.css('.collapsible .collapsible-body')).last().element(by.tagName('button'));
-    var isClickable = EC.elementToBeClickable(el);
-    console.log('waiting for save button to be clickable');
+    var isClickable = EC.elementToBeClickable(el); // need to make sure that the button is clickable before continuing
     browser.wait(isClickable, 5000);
-    console.log('done waiting');
     el.click();
-    }
+  }
+  */
+
+  saveTextLastPractice(){
+    var el = element(by.id('practicesCard')).all(by.css('.collapsible .collapsible-body')).last().element(by.tagName('button'));
+    this.clickWithWait(el);
+  }
+
+  saveTextFirstPractice(){
+    var el = element(by.id('practicesCard')).all(by.css('.collapsible .collapsible-body')).first().element(by.tagName('button'));
+    this.clickWithWait(el);
+  }
+  /*
+  saveTextFirstPractice(){
+    // see: http://stackoverflow.com/questions/30862405/element-is-not-clickable-at-point-protractor
+    var EC = protractor.ExpectedConditions;
+    var el = element(by.id('practicesCard')).all(by.css('.collapsible .collapsible-body')).first().element(by.tagName('button'));
+    var isClickable = EC.elementToBeClickable(el);
+    browser.wait(isClickable, 5000); // need to make sure that the button is clickable before continuing
+    el.click();
+  }
+  */
 
   countPracticesInCard(){// NOTE: returns a promise
     return element(by.id('practicesCard')).all(by.css('.collapsible')).count();
   }
 
-  checkSavedText(){
-    //return element.all(by.id('practiceAdvice')).last().getText();
+  checkSavedTextLastPractice(){
     return element(by.id('practicesCard')).all(by.css('.collapsible .collapsible-body .non-edit-mode')).last().getText();
+  }
+
+  checkSavedTextFirstPractice(){
+    return element(by.id('practicesCard')).all(by.css('.collapsible .collapsible-body .non-edit-mode')).first().getText();
+  }
+
+  fetchAdviceForFirstPractice(){
+    return element(by.id('practicesCard')).all(by.css('.collapsible .collapsible-body .non-edit-mode')).first().getText();
   }
 
   attemptLastPracticeDelete(){//this should launch a modal
@@ -56,6 +91,36 @@ export class AdminEditReadingResourcesPage {
   getModalsOnPage() {
     return element.all(by.css('.modal'));
   }
+
+  /*
+  clickEditPracticeButton() {
+    // see: http://stackoverflow.com/questions/30862405/element-is-not-clickable-at-point-protractor
+    var EC = protractor.ExpectedConditions;
+    var el = element(by.id('practicesCard')).all(by.css('.collapsible .collapsible-body')).first().element(by.linkText('EDIT'));
+    var isClickable = EC.elementToBeClickable(el);
+    browser.wait(isClickable, 5000);
+    el.click();
+  }
+  */
+
+  clickEditPracticeButton() {
+    var el = element(by.id('practicesCard')).all(by.css('.collapsible .collapsible-body')).first().element(by.linkText('EDIT'));
+    this.clickWithWait(el);
+  }
+
+  // see: http://stackoverflow.com/questions/30862405/element-is-not-clickable-at-point-protractor
+  // apparently protractor finds the on-screen coordinates for a given button, but
+  // if there is an animation or a drop-down menu obscuring the button, the given
+  // coordinates might not be 'click-able'; the following makes sure that we
+  // wait until the button is clickable before attempting to click it
+
+  clickWithWait(el) {
+    var EC = protractor.ExpectedConditions;
+    var isClickable = EC.elementToBeClickable(el);
+    browser.wait(isClickable, 5000);
+    el.click();
+  }
+
 
 }
 
