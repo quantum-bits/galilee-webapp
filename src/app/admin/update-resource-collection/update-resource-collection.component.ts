a259d0619079f5b1e0a76d28c1c285ee5f714c15
Ken Kiers <knkiers@taylor.edu>
2016-08-02 17:01:38 -0400

    refactor update-resource-collection component to remove/clean up unused code

--- src/app/admin/update-resource-collection/update-resource-collection.component.ts
+++ src/app/admin/update-resource-collection/update-resource-collection.component.ts
@@ -1,19 +1,9 @@
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
-import {
-  FORM_DIRECTIVES,
-  REACTIVE_FORM_DIRECTIVES,
-  FormBuilder,
-  FormGroup,
-  FormArray,
-  Validators, // used to make a field required
-  FormControl
-} from '@angular/forms';
 
 import { Resource } from '../../shared/interfaces/resource.interface';
 import { ResourceCollection } from '../../shared/interfaces/resource-collection.interface';
 import { UpdateResourceItemComponent } from '../update-resource-item';
 import { UploadResourceComponent } from '../upload-resource';
-
 
 import {MaterializeDirective} from "angular2-materialize";
 
@@ -27,7 +17,6 @@ declare var $: any; // for using jQuery within this angular component
   directives: [
     MaterializeDirective,
     UpdateResourceItemComponent,
-    REACTIVE_FORM_DIRECTIVES,
     UploadResourceComponent],
 })
 export class UpdateResourceCollectionComponent implements OnInit {
@@ -35,46 +24,13 @@ export class UpdateResourceCollectionComponent implements OnInit {
   @Input() resourceCollection: ResourceCollection;
   @Input() resourcePath: string;
 
-  //useful resources:
-  //  https://scotch.io/tutorials/using-angular-2s-model-driven-forms-with-formgroup-and-formcontrol
-  //  https://angular.io/docs/ts/latest/cookbook/dynamic-form.html
-  // *https://scotch.io/tutorials/how-to-build-nested-model-driven-forms-in-angular-2
-  public resourceCollectionForm: FormGroup; // our model driven form
-  public submitted: boolean; // keep track of whether form is submitted
+  constructor() {}
 
-  constructor(private formBuilder: FormBuilder) {}
-
-  ngOnInit() {
-    this.resourceCollectionForm = this.formBuilder.group({
-      title: ['', [<any>Validators.required]],
-      description: ['', [<any>Validators.required]],
-      resources: this.formBuilder.array(
-        this.initResourceArray(this.resourceCollection.resources)),
-    });
-  }
+  ngOnInit() {}
 
   ngOnDestroy() {
     // prevent memory leak when component destroyed
     //this.subscription.unsubscribe(); //not actually using the subscription here....
-  }
-
-  initResourceArray(resources: Resource[]) {
-    let resourceArray = [];
-    for (let resource of resources) {
-      resourceArray.push(this.initResource(resource));
-    }
-    return resourceArray;
-  }
-
-  initResource(resourceInfo: Resource) {
-    // initialize our address
-    return this.formBuilder.group({
-      id: [resourceInfo.id],
-      caption: [resourceInfo.caption, Validators.required],
-      type: [resourceInfo.type, Validators.required],// 'image', 'video', etc.
-      fileName: [resourceInfo.fileName, Validators.required],
-      copyrightInfo: [resourceInfo.copyrightInfo]
-    });
   }
 
   onModalFinished(modalID: string){
@@ -84,5 +40,5 @@ export class UpdateResourceCollectionComponent implements OnInit {
     console.log('#'+modalID);
     $('#'+modalID).closeModal();
   }
-  
+
 }
