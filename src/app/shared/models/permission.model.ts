49de25fc93ae279303fa00c12fc1853c4b0ff298
Ken Kiers <knkiers@taylor.edu>
2016-08-18 17:36:19 -0400

    minor update -- reinstate some e2e tests that were deleted by angular-cli init

--- src/app/shared/models/permission.model.ts
+++ src/app/shared/models/permission.model.ts
@@ -2,6 +2,16 @@ export const EDIT_RES = 'EDIT_RES';
 export const EDIT_PRAC = 'EDIT_PRAC';
 export const ADMIN = 'ADMIN';
 
+/*
+maybe switch to something more like this:
+
+ class PermissionType {
+    static EDIT_RES: string = "EDIT_RES";
+    static EDIT_PRAC: string = "EDIT_PRAC";
+    static ADMIN: string = "ADMIN";
+ }
+ */
+
 export class Permission {
   id: string;
   title: string;
