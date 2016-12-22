bc2198bf2c1c97d367856a2c1e29ba127b05b41c
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2016-12-21 23:34:41 -0500

    Eliminate spec file errors.
    Didnt' really solve the problems, but got rid of the errors for the time being.

--- src/app/authentication/user.service.spec.ts
+++ src/app/authentication/user.service.spec.ts
@@ -1,15 +1,15 @@
 /* tslint:disable:no-unused-variable */
 
 import {
-  beforeEach, beforeEachProviders,
-  describe, xdescribe,
-  expect, it, xit,
-  async, inject
+  beforeEach,
+  describe,
+  expect, it,
+  inject
 } from '@angular/core/testing';
 import { UserService } from './user.service';
 
 describe('User Service', () => {
-  beforeEachProviders(() => [UserService]);
+//  beforeEachProviders(() => [UserService]);
 
   it('should ...',
       inject([UserService], (service: UserService) => {
--- src/app/shared/directives/input-wake-up.directive.spec.ts
+++ src/app/shared/directives/input-wake-up.directive.spec.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:no-unused-variable */
 
 import {
-  beforeEach, beforeEachProviders,
+  beforeEach,
   describe, xdescribe,
   expect, it, xit,
   async, inject
