1ec1dc3011b30ab67dc5c8cce36989a13ad0a4dd
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2017-05-25 23:28:48 -0400

    Clean more cruft as part of package migration.

--- src/app/admin/content-management/bible-info/bible-info.service.ts
+++ src/app/admin/content-management/bible-info/bible-info.service.ts
@@ -1,31 +1,32 @@
-import {Injectable} from "@angular/core";
+import {Injectable} from '@angular/core';
+import {bgBibleData} from './bg-bible-data';
 
 export class BibleBook {
-    constructor(public displayName: string,
-                public osisName: string,
-                public verseCounts: Array<number>,
-                public order: number,
-                public testament: string) {
-    }
+  constructor(public displayName: string,
+              public osisName: string,
+              public verseCounts: Array<number>,
+              public order: number,
+              public testament: string) {
+  }
 }
 
 @Injectable()
 export class BibleInfoService {
-    bibleBooks: Array<BibleBook>;
+  bibleBooks: Array<BibleBook>;
 
-    // Read Bible data and construct BibleBook objects.
-    constructor() {
-        const bibleData = require('./bg-bible-data').data[0].books;
-        this.bibleBooks = bibleData.map(bookData =>
-            new BibleBook(bookData.display, bookData.osis, bookData.verses,
-                bookData.order, bookData.testament));
-    }
+  // Read Bible data and construct BibleBook objects.
+  constructor() {
+    const bibleData = bgBibleData.data[0].books;
+    this.bibleBooks = bibleData.map(bookData =>
+      new BibleBook(bookData.display, bookData.osis, bookData.verses,
+        bookData.order, bookData.testament));
+  }
 
-    defaultBook() : BibleBook {
-        return this.bibleBooks[0];
-    }
+  defaultBook(): BibleBook {
+    return this.bibleBooks[0];
+  }
 
-    findBookByOsisName(osisName): BibleBook {
-        return this.bibleBooks.find(elt => elt.osisName === osisName);
-    }
+  findBookByOsisName(osisName): BibleBook {
+    return this.bibleBooks.find(elt => elt.osisName === osisName);
+  }
 }
