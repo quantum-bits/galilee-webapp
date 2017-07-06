e5f56ff6a367bd3cc83e921c503d0362b4b60176
Ken Kiers <knkiers@taylor.edu>
2017-07-06 10:53:06 -0400

    partial implementation of dragula; not yet working correctly

--- src/app/admin/content-management/update-readings-list/update-readings-list.component.ts
+++ src/app/admin/content-management/update-readings-list/update-readings-list.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit, Input, Output, EventEmitter, ViewChild} from '@angular/core';
 import {Router} from '@angular/router';
 
+import {DragulaService} from 'ng2-dragula/ng2-dragula';
+
 import {DisplayReadingModalComponent} from '../display-reading-modal/display-reading-modal.component';
 import {DeleteItemModalComponent} from '../../../shared/components/delete-item-modal/delete-item-modal.component';
 import {IReading, ReadingDay} from '../../../shared/interfaces/reading.interface';
@@ -45,7 +47,48 @@ export class UpdateReadingsListComponent implements OnInit {
   constructor(private readingService: ReadingService,
               private directionService: DirectionService,
               private practiceService: PracticeService,
+              private dragulaService:DragulaService,
               private router: Router) {
+    dragulaService.drag.subscribe((value) => {
+      console.log(`drag: ${value[0]}`);
+      this.onDrag(value.slice(1));
+    });
+    dragulaService.drop.subscribe((value) => {
+      console.log(`drop: ${value[0]}`);
+      this.onDrop(value.slice(1));
+    });
+    dragulaService.over.subscribe((value) => {
+      console.log(`over: ${value[0]}`);
+      this.onOver(value.slice(1));
+    });
+    dragulaService.out.subscribe((value) => {
+      console.log(`out: ${value[0]}`);
+      console.log(value);
+      this.onOut(value.slice(1));
+    });
+    dragulaService.dropModel.subscribe((value) => {
+      this.onDropModel(value.slice(1));
+    });
+    dragulaService.removeModel.subscribe((value) => {
+      this.onRemoveModel(value.slice(1));
+    });
+
+
+    dragulaService.setOptions('readings-bag', {
+      moves: function (el, container, handle) {
+        //console.log('el: ',el, 'container: ', container, 'handle: ',handle, 'handle.className: ',handle.className);
+        return handle.className === 'handle';
+      },
+      invalid: function (el, handle) {
+        console.log('el: ',el,'el.className: ',el.className, 'handle: ',handle, 'handle.className: ',handle.className);
+        console.log('invalid? ', handle.className === 'invalid-handle' || el.className === 'collection-header');
+        return handle.className === 'invalid-handle' || el.className === 'collection-header';
+      },
+
+
+    });
+
+
   }
 
   ngOnInit() {
@@ -130,5 +173,43 @@ export class UpdateReadingsListComponent implements OnInit {
   navigateToAddReadingPage() {
     this.router.navigate(['/admin/update-readings', this.dateString, this.readingsData.readings.length]);
   }
+
+  private onDrag(args) {
+    let [e, el] = args;
+    console.log('dragged!');
+    // do something
+  }
+
+  private onDrop(args) {
+    let [e, el] = args;
+    console.log('dropped!');
+    // do something
+  }
+
+  private onOver(args) {
+    let [e, el, container] = args;
+    console.log('over!');
+    // do something
+  }
+
+  private onOut(args) {
+    let [e, el, container] = args;
+    console.log('out!');
+    // do something
+  }
+
+  private onDropModel(args) {
+    let [el, target, source] = args;
+    console.log('dropped model!');
+    //console.log(this.practicesThisReading);
+    // do something else
+  }
+
+  private onRemoveModel(args) {
+    let [el, source] = args;
+    console.log('removed model!');
+    // do something else
+  }
+
 
 }
